efc3ab77dde4ee985c481282d60c7d2d2396e0e5
Blessing Olusanya <debbyblessing1234@gmail.com>
2023-10-27 09:23:05 +0100

    Updating the features

--- js/index.js
+++ js/index.js
@@ -3,4 +3,20 @@ const startApp = () => {
   body.innerHTML = '<h2>JAVASCRIPT ENABLED</h2>';
 };
 
+const ruleContainer = document.querySelector('.rule-container');
+const ruleBtn = document.querySelector('.rule-btn');
+const closeRuleDesktop = document.querySelector('.close-rule');
+const closeRuleMobile = document.querySelector('.close-rule2');
+const rule = () => {
+  if (ruleContainer.classList.toggle('active')) {
+    ruleContainer.classList.toggle('is-active');
+  } else {
+    ruleContainer.classList.toggle('is-active');
+  }
+};
+
+ruleBtn.addEventListener('click', rule);
+closeRuleDesktop.addEventListener('click', rule);
+closeRuleMobile.addEventListener('click', rule);
+
 startApp();
